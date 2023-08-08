3b2d46ac825a52c9c128f3a7f22b0b1adda6cd13
mjchoo <mjchoo97@gmail.com>
2023-08-08 23:54:35 +0800

    updated backend api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:8800/api/"
+    
+    // baseURL : "http://localhost:8800/api/"
+    baseURL : "https://moviebuzz-api.onrender.com"
 })
