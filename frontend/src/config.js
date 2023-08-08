dc6cbbb3f5a9752277d27ca0a5b2877094474176
mjchoo <mjchoo97@gmail.com>
2023-08-08 23:58:33 +0800

    missing /api for api endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,5 +3,5 @@ import axios from "axios"
 export const axiosInstance = axios.create({
     
     // baseURL : "http://localhost:8800/api/"
-    baseURL : "https://moviebuzz-api.onrender.com"
+    baseURL : "https://moviebuzz-api.onrender.com/api/"
 })
